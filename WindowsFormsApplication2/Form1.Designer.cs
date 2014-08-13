8b1a0ac3945819dbea6f1718fc8fad787017b8ed
sss22213 <n0404.n0404@gmail.com>
2014-08-13 12:49:56 +0800

    FCOM_C_SHARP

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(82, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(84, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "連接埠:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "傳輸速率:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "停止位元:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "資料位元";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 36);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "開啟連接埠";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(308, 1);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(475, 250);
+            this.textBox4.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(508, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 30);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "開始接收";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(508, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 29);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "傳送";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(403, 310);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(293, 22);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(508, 257);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 30);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "停止接收";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(61, 232);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 36);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "關閉連接埠";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(869, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 12);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "已傳送0筆資料";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(869, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 12);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "已接收0筆資料";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 404);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "FCOM (C#)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApplication1
+    { 
+    
+    public partial class Form1 : Form
+    {
+
+
+        SerialPort port=new SerialPort();
+        int rx, tx=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form.CheckForIllegalCrossThreadCalls =false;
+
+            foreach (String port1 in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(port1);
+
+            }
+
+        }
+
+        private void textBox5_Keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                MessageBox.Show("ss");
+                button3.PerformClick();
+
+
+            }
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((comboBox1.Text).Length < 1 || (textBox1.Text).Length < 1 || (textBox2.Text).Length < 1 || (textBox3.Text).Length < 1)
+                {
+                    MessageBox.Show("請確認資料是否完整");
+                    return;
+
+
+                }
+                else
+                {
+                    port.PortName = comboBox1.Text;
+                    port.BaudRate = Convert.ToInt32(textBox1.Text);
+                    port.DataBits = Convert.ToInt32(textBox3.Text);
+                    port.Parity = Parity.None;
+                    if (Convert.ToInt32(textBox2.Text) == 1)
+                    {
+                        port.StopBits = StopBits.One;
+                    }
+                    else if (Convert.ToInt32(textBox2.Text) == 2)
+                    {
+                        port.StopBits = StopBits.Two;
+                    }
+                    else
+                    {
+                        MessageBox.Show("只可以為1或是2");
+                        return;
+
+
+                    }
+                    port.Open();
+                    button1.Visible = false;
+                    button5.Visible = true;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                }
+               
+
+            }
+             catch(Exception ex)
+                {
+                    MessageBox.Show("發生錯誤，請聯絡你的系統管理員");
+                    return;
+
+                
+                }
+
+
+
+
+
+
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+           
+                string sk = "";
+               
+                byte[] s1 = new byte[port.BytesToRead];
+                port.Read(s1, 0, s1.Length);
+                for (int i = 0; i < s1.Length;i++ )
+                {
+                    sk += s1[i];
+
+                }
+
+                if (sk.Length > 0)
+                {
+                  textBox4.Text = textBox4.Text + "\r\n" + DateTime.Now +": "+String.Format("{0:X}",Convert.ToInt16(sk)) + "\r\n";
+                 textBox4.SelectionStart = textBox4.Text.Length;
+                 textBox4.ScrollToCaret();
+                 rx += 1;
+                 label6.Text = "已接收" + rx + "筆資料";
+
+                }
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 10;
+
+
+
+            button2.Visible = false;
+            button4.Visible = true;
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy == false)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //  sixty = (TextBox3.Text).Length
+            //If Not sixty = 2 Then
+            //    MsgBox("數字輸入錯誤")
+            //    Return
+            //Else
+            //    For i As Integer = 1 To sixty
+
+            //        k1 = Val(GetChar(TextBox3.Text, i))
+            //        k1 = sixtygetten(k1, i)
+            //        SUM = (k1 * 16 ^ (sixty - i)) + SUM
+
+            //    Next
+            //End If
+            //
+
+            int sixty = 0;
+            string g1 = textBox5.Text;
+            int k1 = 0;
+            int SUM =0;
+            sixty = (textBox5.Text).Length;
+            if (sixty != 2)
+            {
+                MessageBox.Show("輸入錯誤");
+                return;
+
+            }
+            else
+            {
+                for (int i = 0; i < sixty; i++)
+                {
+                    string g2 = g1.Substring(i, 1);
+                    if (g2 == "A" || g2 == "B" || g2 == "C" || g2 == "D" || g2 == "E" || g2 == "F")
+                    {
+                        k1 = sixthget(i);
+                    }
+                    else
+                    {
+                        k1 = Convert.ToInt16(g1.Substring(i, 1));
+                    }
+
+
+                    SUM = Convert.ToInt16((k1 * Math.Pow(16,1-i)) )+ SUM;
+                   
+                }
+
+            }
+            
+       
+            byte[] buffer = new byte[1];
+            buffer[0] = Convert.ToByte(SUM);
+            port.Write(buffer, 0, buffer.Length);
+            tx += 1;
+            label5.Text = "已傳送" + tx + "筆資料";
+            textBox5.Text = "";
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (backgroundWorker1.IsBusy == true)
+            {
+                backgroundWorker1.CancelAsync();
+
+            
+            }
+            button3.Enabled = false;
+            button4.Visible = false;
+            button2.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button4.PerformClick();
+            button1.Visible = true;
+            button2.Enabled = false;
+            button5.Visible = false;
+            port.Close();
+
+        }
+
+      public int sixthget(int i)
+        {
+            string g1 = textBox5.Text;
+            int k1=0;
+            if (g1.Substring(i, 1) == "A")
+            {
+                k1 = 10;
+            }
+            if (g1.Substring(i, 1) == "B")
+            {
+                k1 = 11;
+            }
+            if (g1.Substring(i, 1) == "C")
+            {
+                k1 = 12;
+            }
+            if (g1.Substring(i, 1) == "D")
+            {
+                k1 = 13;
+            }
+            if (g1.Substring(i, 1) == "E")
+            {
+                k1 = 14;
+            }
+            if (g1.Substring(i, 1) == "F")
+            {
+                k1 = 15;
+            }
+
+            return k1;
+
+        
+        }
+
+      private void textBox5_TextChanged(object sender, EventArgs e)
+      {
+
+      }
+
+    }
+  
+
+}
